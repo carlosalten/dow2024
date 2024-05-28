257bc91efdc2534504e8944a0f1d2dc92c2f2c81
carlosalten <carlos.alten@usm.cl>
2024-05-28 10:52:30 -0400

    Preparación clase vista detalle

--- ejemplo_futbol/resources/views/partidos/show.blade.php
+++ ejemplo_futbol/resources/views/partidos/show.blade.php
@@ -1,53 +1,73 @@
 @extends('templates.master')
 
 @section('contenido-principal')
-<div class="row mt-3">
-    <div class="col">
-        <h3>Resultado Partido</h3>
+<div class="container-fluid">
+    <div class="row mt-3">
+        <div class="col">
+            <h3>Resultado Partido</h3>
+        </div>
     </div>
-</div>
 
-<div class="row">
-    <div class="col">
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-3">
-                        <img class="img-fluid rounded" src="{{ Storage::url($partido->estadio->imagen) }}">
-                    </div>
-                    <div class="col-4">
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item"><strong>Fecha: </strong>{{ $partido->fecha }}</li>
-                            <li class="list-group-item"><strong>Estadio: </strong>{{ $partido->estadio->nombre }}</li>
-                            <li class="list-group-item"><strong>Equipo Local: </strong>{{ $partido->equipos->where('pivot.es_local',true)->first()->nombre }}</li>
-                            <li class="list-group-item"><strong>Equipo Visita: </strong>{{ $partido->equipos->where('pivot.es_local',false)->first()->nombre }}</li>
-                            <li class="list-group-item"><strong>Estado: </strong>{{ $partido->jugado==0?'Pendiente':'Finalizado' }}</li>
-                        </ul>
-                    </div>
-                    <div class="col-5">
-                        <h4>Equipo Local</h4>
-                        <form>
-                            <div class="mb-3">
-                                <label for="nombre" class="form-label">Nombre</label>
-                                <input type="number" id="nombre" name="nombre" class="form-control">
-                            </div>
-                        </form>
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        {{-- datos partido --}}
+                        <div class="col-12 col-lg-6 mb-3 mb-lg-0">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item"><strong>Fecha: </strong>28-05-2024</li>
+                                <li class="list-group-item"><strong>Estadio: </strong>Nacional Julio Martínez</li>
+                                <li class="list-group-item"><strong>Equipo Local: </strong>Audax Italiano</li>
+                                <li class="list-group-item"><strong>Equipo Visita: </strong>Universidad de Chile</li>
+                                <li class="list-group-item"><strong>Estado: </strong>Pendiente</li>
+                                <li class="list-group-item"><strong>Resultado: 0-0</strong></li>
+                            </ul>
+                        </div>
+                        {{-- /datos partido --}}
+
+                        {{-- imagen estadio --}}
+                        <div class="col-12 col-lg-3 order-lg-first mb-3 mb-lg-0">
+                            <img class="img-fluid rounded" src="/storage/estadios/YYZsdTSwavlJTB8hVOr7viP04D3gRjwISjE4ZzLR.jpg">
+                        </div>
+                        {{-- /imagen estadio --}}
+
+                        {{-- formulario modificar resultado --}}
+                        <div class="col-12 col-lg-3">
+                            <h4>Goles</h4>
+                            <form>
+                                <div class="mb-3">
+                                    <label for="equipo_local" class="form-label">Audax Italiano</label>
+                                    <input type="number" id="equipo_local" name="equipo_local" class="form-control" value="0">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="equipo_visita" class="form-label">Universidad de Chile</label>
+                                    <input type="number" id="equipo_visita" name="equipo_visita" class="form-control" value="0">
+                                </div>
+                                <div class="mb-3 d-grid gap-2 d-lg-block text-end">
+                                    <button type="submit" class="btn btn-success">Modificar Resultados</button>
+                                </div>
+                            </form>
+                        </div>
+                        {{-- /formulario modificar resultado --}}
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
-<div class="row mt-3">
-    <div class="col">
-        <div class="card">
-            <div class="card-body">
-                <div class="d-grid gap-2 d-lg-block">
-                    <a href="{{ route('partidos.index') }}" class="btn btn-warning">Volver a Partidos</a>
+    {{-- boton volver a partidos --}}
+    <div class="row mt-3">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-grid gap-2 d-lg-block">
+                        <a href="http://localhost:8000/partidos" class="btn btn-warning">Volver a Partidos</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+    {{-- /boton volver a partidos --}}
 </div>
 @endsection
